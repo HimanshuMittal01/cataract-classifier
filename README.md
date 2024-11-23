d4181359d0ec463b9f3a26bbc919dad306454655
Himanshu Mittal <himanshu.mittal.dev@gmail.com>
2024-11-23 14:11:45 +0530

    add: initial setup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cataract-classifier
 Binary classification model to identify cataracts in images deployed via FASTAPI
+
+## How to run
+
+1. Create python environment
+
+If you are using `uv`:
+```bash
+uv venv --python 3.10
+source .venv/bin/activate
+```
+
---
+++ cataract_classifier/utils.py
@@ -0,0 +1,33 @@
+"""
+Contains utility functions to handle file information
+"""
+
+from collections import defaultdict
+
+from rich import print
+from rich.table import Table
+
+
+def sizeof_fmt(num, suffix="B"):
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f} {unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f} Yi{suffix}"
+
+
+def display_dir_items(path):
+    table = Table()
+    table.add_column("Type", justify="right", no_wrap=True)
+    table.add_column("Path", style="cyan")
+    table.add_column("Size", justify="right", style="green")
+
+    filetype_counter = defaultdict(int)
+    for filepath in path.glob("*"):
+        pathtype = "File" if filepath.is_file() else "Folder"
+        filetype_counter[pathtype] += 1
+        filesize = filepath.stat().st_size
+        table.add_row(pathtype, str(filepath), sizeof_fmt(filesize))
+
+    table.title = f"{filetype_counter['File']} files, {filetype_counter['Folder']} folders"
+    print(table)
